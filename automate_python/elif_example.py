3a58a52ffa72382721f903de4bcbeac689ee66db
kmd <krunal.darji@outlook.com>
2020-04-07 12:24:34 -0400

    more python practice

---
+++ automate_python/elif_example.py
@@ -0,0 +1,10 @@
+name = 'Bob'
+age = 3000
+if name == 'Alice':
+    print('Hi Alice')
+elif age < 12:
+    print('You are not Alice')
+elif age > 2000:
+    print('Alice is not an undead')
+elif age > 100:
+    print('You are not Alice, grannie.')
